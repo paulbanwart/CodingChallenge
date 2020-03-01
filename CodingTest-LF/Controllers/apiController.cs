fba848c8ac80d2e1c054782bdd655cf07a4b53ad
Paul <paul.banwart@gmail.com>
2020-02-29 19:37:05 -0600

    added documentation

--- CodingTest-LF/Controllers/apiController.cs
+++ CodingTest-LF/Controllers/apiController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using CodingTest_LF.Models;
@@ -14,11 +15,13 @@ namespace CodingTest_LF.Controllers
     {
         char[] alphabet = new char[26] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
         // POST: api/encode
+        //Due to this class being an APIController and this method being an HttpPost there are alot of goodies baked in like responding with a 500 when there is a server error
         [HttpPost]
-        public string Post([FromBody] Cipher cipher)
+        public EncodedMessageModel Post([FromBody] Cipher cipher)
         {
-           char[] encodedArray = new char[cipher.Message.Length];
-           string message = cipher.Message.ToUpper();
+            char[] encodedArray = new char[cipher.Message.Length];
+            //uppercase all characters so we only need to test against 26 total characters
+            string message = cipher.Message.ToUpper();
 
             //go letter by letter through the alphabet, updating the encodedArray when there's a match
             for (int i = 0; i < alphabet.Length; i++)
@@ -26,22 +29,22 @@ namespace CodingTest_LF.Controllers
                 int count = 0;
                 foreach (char c in message)
                 {
-                 //ignore spaces, preserve its place in array
+                    //ignore spaces, preserve its place in array
                     if (c == ' ')
                     {
                         encodedArray[count] = ' ';
                         count++;
                         continue;
                     }
-                  //ignore any case where the alphabet character does not equal, don't do anything, continue       
+                    //ignore any case where the alphabet character does not equal, don't do anything, continue       
                     if (alphabet[i] != c)
                     {
                         count++;
                         continue;
                     }
 
-                    //calculate the shifted letter using the index of the alphabet array
-                   int shift = Array.IndexOf(alphabet, c) + cipher.Shift;
+                    //calculate the shifted letter using the index of the alphabet array to get offset
+                    int shift = Array.IndexOf(alphabet, c) + cipher.Shift;
                     if (shift <= 0)
                         shift = shift + 26;
 
@@ -50,11 +53,26 @@ namespace CodingTest_LF.Controllers
 
                     //update encodedArray with encoded character
                     encodedArray[count] = alphabet[shift];
+                    //next letter of the alphabet
                     count++;
                 }
             }
-            //save encoded message to file (append new line with encoded message)
-            return new string(encodedArray);
+            EncodedMessageModel encodedMessage = new EncodedMessageModel() { EncodedMessage = new string(encodedArray) };
+            //save encoded message to file (i.e. append new line with encoded message)
+           bool Saved = SaveToFile(new EncodedMessageModel() { EncodedMessage = new string(encodedArray)});
+            if (!Saved)
+            {
+                //if unable to save file, respond with empty string
+                return new EncodedMessageModel() { EncodedMessage = "" };
+            }
+            return encodedMessage;
+        }
+
+        private bool SaveToFile(EncodedMessageModel encodedMessage)
+        {
+
+            return true;
+
         }
     }
 }
